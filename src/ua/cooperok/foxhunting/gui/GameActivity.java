7e6bd4409fd41bec40c879c8efeecb1e8f7bb804
cooperok <cooperok.so@gmail.com>
2012-11-29 14:44:59 +0200

    Made a simple start page

---
+++ src/ua/cooperok/foxhunting/gui/GameActivity.java
@@ -0,0 +1,18 @@
+package ua.cooperok.foxhunting.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import ua.cooperok.foxhunting.R;
+
+public class GameActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.game_layout);
+	
+	}
+	
+}
---
+++ src/ua/cooperok/foxhunting/gui/MainActivity.java
@@ -0,0 +1,63 @@
+package ua.cooperok.foxhunting.gui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import ua.cooperok.foxhunting.R;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);
+		
+		addListeners();
+			
+	}
+	
+	protected void addListeners() {
+		
+		//Adding listeners on click on every button
+		findViewById(R.id.button_new).setOnClickListener(this);
+		
+		findViewById(R.id.button_records).setOnClickListener(this);
+		
+		findViewById(R.id.button_settings).setOnClickListener(this);
+		
+		findViewById(R.id.button_about).setOnClickListener(this);
+		
+	}
+	
+	/**
+	 * Listener on buttons click
+	 */
+	public void onClick(View v) {
+
+		switch(v.getId()) {
+		
+			case R.id.button_new:
+				
+				Intent intent = new Intent(this, GameActivity.class);
+				
+				startActivity(intent);
+				
+				break;
+				
+			//@TODO implement every listener
+			default:
+				
+				Toast.makeText(getApplicationContext(), ((Button)v).getText(), Toast.LENGTH_SHORT).show();
+				
+				break;
+		
+		}
+		
+	}
+	
+}
